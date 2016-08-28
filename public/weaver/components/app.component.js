1b6614522320381f6f4cbdd33a86afe9aa230b04
lilian and jason <jl@lilians-MacBook-Air.local>
2016-08-28 21:03:12 +0800

    2016-08-28a

--- public/weaver/components/app.component.js
+++ public/weaver/components/app.component.js
@@ -15,7 +15,11 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>first</h1> app'
+            /*
+             *  bug? seems the "weaver" variable in System.js is not working in here,
+             *      hence you need to supply the full path to the the template's url
+             */
+            templateUrl: 'public/weaver/components/views/app.component.html'
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- public/weaver/components/app.component.ts
+++ public/weaver/components/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>first</h1> app'
+    /*
+     *  bug? seems the "weaver" variable in System.js is not working in here, 
+     *      hence you need to supply the full path to the the template's url
+     */
+    templateUrl: 'public/weaver/components/views/app.component.html'
 })
 
 export class AppComponent {};
